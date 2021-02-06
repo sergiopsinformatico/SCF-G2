c6a49e6aef20108f4011ef6d1d583206e8aa8dc6
Jesús Javier Catalán Ramírez <jesusjcatalan@gmail.com>
2021-02-06 20:08:46 +0100

    InfluxDB Aemet DB with retention policy
    New database for aemet data. Created retention policy 1h.
    Minor changes on aemet flow

--- resources/influxdb/create-database.bat
+++ resources/influxdb/create-database.bat
@@ -8,7 +8,12 @@ IF NOT "%CONTINUE%"=="y" (exit /b 0)
 
 
 curl -i "http://localhost:8086/query" --data-urlencode "q=DROP DATABASE residentAngel" 
+curl -i "http://localhost:8086/query" --data-urlencode "q=DROP DATABASE aemet" 
+
 curl -i "http://localhost:8086/query" --data-urlencode "q=CREATE DATABASE residentAngel" 
+curl -i "http://localhost:8086/query" --data-urlencode "q=CREATE DATABASE aemet" 
+REM Politica de retención de datos en bdd aemet. 1 Hora.
+curl -i "http://localhost:8086/query" --data-urlencode "q=CREATE RETENTION POLICY aemet_retention ON aemet DURATION 1h REPLICATION 1" 
 curl -i -XPOST "http://localhost:8086/write?db=residentAngel" --data-binary @residences.data
 
 curl -i http://localhost:8086/query --data-urlencode "db=residentAngel" --data-urlencode "q=SELECT * FROM residences"
