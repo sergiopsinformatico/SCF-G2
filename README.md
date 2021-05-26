ed0d0f4c3c78fb805ac16338f621e525bf23737f
Jesús Javier Catalán Ramírez <jesusjcatalan@gmail.com>
2021-05-26 19:54:09 +0200

    Fixed versions. More readme.md

---
+++ README.md
@@ -0,0 +1,13 @@
+# Proyect Setup
+
+1. Run win-setup.bat
+2. Run deploy-containers.bat * 
+3. Connect to node red container and install 
+
+> npm install node-red-contrib-protobuf --production
+
+4. En el contenedor de nodeRed copiar el fichero environment.proto
+
+5. Reiniciar nodeRed e importar los flujos.
+
+
--- resources/docker/deploy-containers.bat
+++ resources/docker/deploy-containers.bat
@@ -6,16 +6,16 @@ if "%BROKER_IP%" == "" (
     exit /b 1
 )
 
-docker pull nodered/node-red
-docker pull influxdb
+docker pull nodered/node-red:1.2.7
+docker pull influxdb:1.8.3
 docker pull grafana/grafana:6.5.0-ubuntu
-docker pull eclipse-mosquitto
+docker pull eclipse-mosquitto:1.6.14
 
 docker network create mybridge
-docker run -p 8086:8086 --name=influxdb --net=mybridge influxdb
-docker run -p 2883:1883 --name=broker --net=mybridge eclipse-mosquitto
-docker run -p 3000:3000 --name=grafana --net=mybridge grafana/grafana:6.5.0-ubuntu
-docker run -it -p 1880:1880 -v node_red_data:/data ^
+docker run -d -p 8086:8086 --name=influxdb --net=mybridge influxdb:1.8.3
+docker run -d -p 2883:1883 --name=broker --net=mybridge eclipse-mosquitto:1.6.14
+docker run -d -p 3000:3000 --name=grafana --net=mybridge grafana/grafana:6.5.0-ubuntu 
+docker run -d -it -p 1880:1880 -v node_red_data:/data ^
 -e TZ=Europe/Madrid ^
 -e BROKER_IP=%BROKER_IP% ^
 -e INFLUX_IP=%BROKER_IP% ^
